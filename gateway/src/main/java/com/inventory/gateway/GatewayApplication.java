76971d51cd558293abca2cdb60940d0fb81f5ae1
MNAJULAG7 <manjula717171@gmail.com>
2026-07-16 07:49:48 +0530

    add cors in gatway

--- gateway/src/main/java/com/inventory/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/inventory/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class GatewayApplication {
 
 	public static void main(String[] args) {
